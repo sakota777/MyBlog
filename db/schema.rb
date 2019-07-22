467f48069142d080ec46961669d493d935e49be2
Ryohei Sakota <ryoheisakota@RyoheinoMacBook-puro.local>
2019-07-22 14:54:39 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_071904) do
+ActiveRecord::Schema.define(version: 2019_07_22_055251) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
